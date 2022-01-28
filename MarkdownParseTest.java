39c6e2928c3329c1ed574a12fbe3ca56b9190307
ShelFrench <sheldofrench@gmail.com>
2022-01-27 23:25:51 -0800

    test added

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,38 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void firstTest() throws IOException{
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com", "some-page.html"),links);
+    }
+
+    @Test
+    public void secondTest() throws IOException{
+        Path fileName = Path.of("firstline.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("thisplace.com"),links);
+    }
+
+    @Test
+    public void thirdTest() throws IOException{
+        Path fileName = Path.of("noParen.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals(List.of("https://something.com", "some-page.html"),links);
     }
 }
