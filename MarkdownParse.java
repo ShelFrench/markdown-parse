6d22628ccbfc5cdc1839101c6226876c15f56ec2
ShelFrench <sheldofrench@gmail.com>
2022-02-02 15:24:44 -0800

    yada

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -21,7 +21,7 @@ public class MarkdownParse {
                 continue;
             }
             if(openParen == -1){
-                return toReturn;
+                currentIndex = closeParen +1;
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             //System.out.println(markdown.charAt(currentIndex));
